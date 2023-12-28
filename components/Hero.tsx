137dee8c2f3e1acf55d3bc2154fd5f04952f75d2
tby23rd <tabitha.maru@gmail.com>
2023-12-28 15:05:51 -0700

    more routing issues fixed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero: React.FC = () => {
  
   const handleShopNow = () => {
     // Programmatic navigation
-    window.location.href = '/Shop';
+    window.location.href = '/OrderPage';
   };
 
   const handleCatalog = () => {
